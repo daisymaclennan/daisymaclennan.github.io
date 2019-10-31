aa13c2d7f661d657763d79cd35a25663ba281df5
Daisy Maclennan <daisy@sidigital.co>
2019-10-31 15:46:35 +0000

    Revert "mistake"
    This reverts commit 15b58ef5dc3d70ad97d82fd701bf2c3f53ee62b4.

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "10a1e3b28e1ea38dd84b";
+/******/ 	var hotCurrentHash = "bf626a42724a8c2cf416";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
